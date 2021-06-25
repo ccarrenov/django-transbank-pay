b2d8951df14911c0dc049c2db838b5d0dbffcd82
Cristian Carreño <cristian_c_v@hotmail.com>
2021-06-25 17:47:09 -0400

    v1.1.0: separación archivo settings default + prod

--- bazarw_front_end/bazarw_front_end/deploy/prod/settings.py
+++ bazarw_front_end/bazarw_front_end/deploy/prod/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
@@ -139,6 +139,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Activate Django-Heroku
-django_heroku.settings(locals())
