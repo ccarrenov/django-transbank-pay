93f9b1b8e006f73c8c7dd192432e1564a397b37f
Cristian Carreño <cristian_c_v@hotmail.com>
2021-06-25 14:24:52 -0400

    1.0.0-beta: config heroku

--- bazarw_front_end/bazarw_front_end/settings.py
+++ bazarw_front_end/bazarw_front_end/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
