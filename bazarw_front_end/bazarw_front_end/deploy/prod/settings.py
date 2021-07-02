ff8fb4114a4d167a8a08a7f729b22c4796c4c30a
Cristian Carreño <cristian_c_v@hotmail.com>
2021-07-01 22:08:16 -0400

    se agrega flujo de rechazo

--- bazarw_front_end/bazarw_front_end/deploy/prod/settings.py
+++ bazarw_front_end/bazarw_front_end/deploy/prod/settings.py
@@ -130,11 +130,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, PROJECT_NAME, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, PROJECT_NAME, 'static'),
+#)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
