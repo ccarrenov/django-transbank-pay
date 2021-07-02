c8611582606f7671639704bc30185d31bff7e927
Cristian Carreño <cristian_c_v@hotmail.com>
2021-07-01 22:32:37 -0400

    se agrega flujo de rechazo

--- bazarw_front_end/bazarw_front_end/deploy/prod/settings.py
+++ bazarw_front_end/bazarw_front_end/deploy/prod/settings.py
@@ -131,11 +131,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, PROJECT_NAME, 'static'),
-#)
-
-WHITENOISE_USE_FINDERS = True
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
