01f8433f6ca21b6166917662bc8c165725237750
Cristian Carreño <cristian_c_v@hotmail.com>
2021-07-02 00:46:32 -0400

    update directory

--- bazarw_front_end/deploy/prod/settings.py
+++ bazarw_front_end/deploy/prod/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
